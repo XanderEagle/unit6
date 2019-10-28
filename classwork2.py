763b900d8c25f5e0aa29af5d21def72b6ec2cb33
Xander <alexander.eagle@ssfs.org>
2019-10-28 11:19:51 -0400

    made progress in the classwork activites

---
+++ classwork2.py
@@ -0,0 +1,11 @@
+def are_duplicates(nums):
+    for x in nums:
+        for y in nums[x + 1]:
+            if y == x:
+                return True
+    return False
+
+
+are_duplicates = [1, 2, 3, 4, 5]
+print(are_duplicates)
+
