d65c9ff8927aac243f65dda935bf843863e5b22f
Xander <alexander.eagle@ssfs.org>
2019-10-29 11:59:42 -0400

    did more classwork activites

---
+++ classwork3.py
@@ -0,0 +1,4 @@
+test = 5
+
+for num in range(1, 13):
+    print(num, "x", test, "=", (num * test))
---
+++ classwork4.py
@@ -0,0 +1,8 @@
+def add_numbers(nums):
+    total = 0
+    for x in nums:
+        total = x + total
+    return total
+
+
+print(add_numbers([1, 2, 3, 4, 5, 6, 100, 123]), "is the total sum of the list")
